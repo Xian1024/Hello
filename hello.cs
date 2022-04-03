d64786d01136823353ef481d6e482d18608c9bc7
DevWiki <devwiki2014@gmail.com>
2022-04-03 23:36:41 +0800

    add c# file

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Xian1024
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
